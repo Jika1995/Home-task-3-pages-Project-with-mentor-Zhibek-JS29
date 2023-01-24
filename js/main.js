781b9c32741edc977f6507e6b2ac1cf96a01fc07
Jibek Joldoshbaeva <lajika@MacBook-Air-Jibek.local>
2023-01-24 22:17:07 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// account logic
+// show modal logic
+
+// connecting to elements
+let registerUserModalBtn = document.querySelector('#registerUser-modal');
+let loginUserModalBtn = document.querySelector('#loginUser-modal');
+let registerUserModalBlock = document.querySelector('#registerUser-block');
+let loginUserModalBlock = document.querySelector('#loginUser-block');
+let registerUserBtn = document.querySelector('#registerUser-btn');
+let loginUserBtn = document.querySelector('#loginUser-btn');
+let logoutUserBtn = document.querySelector('#logoutUser-btn');
+// console.log(registerUserModalBtn, loginUserModalBtn, registerUserModalBlock, loginUserModalBlock, registerUserBtn, loginUserBtn, logoutUserBtn);
+
+//modal change logic
+
+registerUserModalBtn.addEventListener('click', () => {
+    registerUserModalBlock.setAttribute('style', 'display: flex !important;');
+    registerUserBtn.setAttribute('style', 'display: flex !important;');
+    loginUserModalBlock.setAttribute('style', 'display: none !important;');
+    loginUserBtn.setAttribute('style', 'display: none !important;');
+});
+
+loginUserModalBtn.addEventListener('click', () => {
+    loginUserModalBlock.setAttribute('style', 'display: flex !important;');
+    loginUserBtn.setAttribute('style', 'display: flex !important;');
+    registerUserModalBlock.setAttribute('style', 'display: none !important;');
+    registerUserBtn.setAttribute('style', 'display: none !important;');
+});
+
